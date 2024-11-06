eb391dc15d24f00b7331852bee28947c2599c618
Sadia <sadiaakther@sadias-MBP.attlocal.net>
2024-11-05 20:51:03 -0500

    homepage

---
+++ homepage.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jeopardy Game</title>
+    <link rel="stylesheet" href="hp.css">
+</head>
+<body>
+    <div class="top-links">
+        <a href="tutorial.php">Tutorial</a>
+        <a href="index.php">Game</a>
+    </div>
+
+    <div class="welcome-container">
+        <h1>Welcome to Jeopardy</h1>
+    </div>
+</body>
+</html>
